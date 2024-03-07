ba638aaf4c21d140aa2d67bd3be503038bd4146c
Yan <Exm230600@gmail.com>
2024-03-07 11:22:42 +0100

    .

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ window.onload = function() {
 
 function checkPassword() {
   var password = document.getElementById('password').value;
-  // Проверяем пароль
+
   if (password === 'qMpoF84HdUd') {
-    // Если пароль верный, скрываем модальное окно
+ 
     modal.style.display = 'none';
   } else {
-    // Если пароль неверный, выводим сообщение об ошибке
+ 
     alert('Неверный пароль. Попробуйте еще раз.');
   }
 }
 
-// Получаем форму для пароля
+
 var passwordForm = document.getElementById('passwordForm');
 
-// Добавляем обработчик события для отправки формы
+
 passwordForm.addEventListener('submit', function(event) {
-  // Отменяем стандартное действие отправки формы
+
   event.preventDefault();
-  // Проверяем пароль
   checkPassword();
 });
